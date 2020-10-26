f611833cd195ae8f1768354a917bae5881d69f4d
chandrakumar <osaimar19@gmail.com>
2020-10-26 09:23:33 +0530

    update app specifi. logg

--- src/main/java/com/chstore/ca/employee/CaEmployeeApplication.java
+++ src/main/java/com/chstore/ca/employee/CaEmployeeApplication.java
@@ -1,11 +1,16 @@
 package com.chstore.ca.employee;
 
 import com.chstore.ca.ms.tracking.CHRequestContext;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
@@ -22,7 +27,6 @@ public class CaEmployeeApplication {
     public String helloWorld() {
 
         log.info("{}", Objects.isNull(chRequestContext));
-        System.out.println(Objects.isNull(chRequestContext));
         return "Hello world and 'loaded spring factory bean ='" + Objects.nonNull(chRequestContext);
     }
 
@@ -31,8 +35,20 @@ public class CaEmployeeApplication {
         throw new RuntimeException("thrown exception");
     }
 
+    @PostMapping("/")
+    public void postHelloWorld(@RequestBody Test test) {
+        log.info("{}", Objects.nonNull(test));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CaEmployeeApplication.class, args);
     }
 
+}
+
+@Setter
+@Getter
+@NoArgsConstructor
+class Test {
+    private String name;
 }
